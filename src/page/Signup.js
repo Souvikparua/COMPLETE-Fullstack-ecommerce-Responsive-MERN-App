4a59872d66e63d03752e0312dbeea85d6849c054
Aditi Singh <aditidhn06@gmail.com>
2024-02-28 18:54:06 +0530

    Signup file #29

--- src/page/Signup.js
+++ src/page/Signup.js
@@ -3,152 +3,201 @@ import { Link, useNavigate } from "react-router-dom";
 import loginSignupImage from "../assest/login-animation.gif";
 import { BiShow, BiHide } from "react-icons/bi";
 import { BsEmojiSmileUpsideDown } from "react-icons/bs";
-import {toast} from "react-hot-toast";
+import { toast } from "react-hot-toast";
 import { useDispatch } from "react-redux";
 import { loginRedux } from "../redux/userSlice";
 
 const Signup = () => {
   const [showPassword, setShowPassword] = useState(false);
-  const [formData, setFormData] = useState({
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const [data, setData] = useState({
     firstname: "",
     lastname: "",
     username: "",
     email: "",
     password: "",
+    confirmPassword:"",
     address: "",
-    phone: ""
+    phone: "",
+    image: "",
   });
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
   const handleShowPassword = () => {
-    setShowPassword(prev => !prev);
+    setShowPassword((prev) => !prev);
+  };
+  const handleShowConfirmPassword = () => {
+    setShowConfirmPassword((preve) => !preve);
   };
 
   const handleOnChange = (e) => {
-    setFormData({
-      ...formData,
-      [e.target.name]: e.target.value
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
     });
   };
 
+  const handleUploadProfileImage = async(e)=>{
+
+    setData((preve)=>{
+        return{
+          ...preve,
+          image : data
+        }
+    })
+
+}
+
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await fetch("http://localhost:8080/api/user/signup", {
+    const { firstname, email, password, confirmPassword } = data;
+    if (firstname && email && password && confirmPassword) {
+      if (password === confirmPassword) {
+      const fetchData = await fetch("http://localhost:8080/api/user/signup", {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(data),
       });
-      const data = await response.json();
-      if (response.ok) {
-        dispatch(loginRedux(data));
+
+      const dataRes = await fetchData.json();
+
+      if (dataRes.ok) {
+  
         toast.success("Registration successful!");
-        setTimeout(() => {
-          navigate("/login"); 
-        }, 1000);
+        
+          navigate("/login");
+       
       } else {
         toast.error("Registration failed!");
       }
-    } catch (error) {
-      console.error("Error:", error);
-      toast.error("An error occurred. Please try again."); 
-    }
+    } }
   };
 
   return (
     <div className="p-3 md:p-4">
-      <div className="w-full max-w-sm bg-white m-auto flex flex-col p-4">
-        <div className="w-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto">
-          <img src={loginSignupImage} className="w-full" alt="Signup" />
+      <div className="w-full max-w-sm bg-white m-auto flex  flex-col p-4">
+        {/* <h1 className='text-center text-2xl font-bold'>Sign up</h1> */}
+        <div className="w-20 h-20 overflow-hidden rounded-full drop-shadow-md shadow-md m-auto relative ">
+          <img src={data.image ? data.image :  loginSignupImage} className="w-full h-full" />
+
+          <label htmlFor="profileImage">
+            <div className="absolute bottom-0 h-1/3  bg-slate-500 bg-opacity-50 w-full text-center cursor-pointer">
+              <p className="text-sm p-1 text-white">Upload</p>
+            </div>
+            <input type={"file"} id="profileImage" accept="image/*" className="hidden" onChange={handleUploadProfileImage}/>
+          </label>
         </div>
+
         <form className="w-full py-3 flex flex-col" onSubmit={handleSubmit}>
-          <label htmlFor="firstname">First Name</label>
+          <label htmlFor="firstName">First Name</label>
           <input
-            type="text"
+            type={"text"}
             id="firstname"
             name="firstname"
-            value={formData.firstname}
-            onChange={handleOnChange}
             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            required
+            value={data.firstname}
+            onChange={handleOnChange}
           />
-          <label htmlFor="firstname">Last Name</label>
+
+          <label htmlFor="lastname">Last Name</label>
           <input
-            type="text"
+            type={"text"}
             id="lastname"
             name="lastname"
-            value={formData.lastname}
-            onChange={handleOnChange}
             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            required
+            value={data.lastname}
+            onChange={handleOnChange}
           />
-          <label htmlFor="firstname">User Name</label>
+          <label htmlFor="username">Username</label>
           <input
-            type="text"
+            type={"text"}
             id="username"
             name="username"
-            value={formData.username}
-            onChange={handleOnChange}
             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            required
+            value={data.username}
+            onChange={handleOnChange}
           />
-          <label htmlFor="firstname">Your Email</label>
+
+          <label htmlFor="email">Email</label>
           <input
-            type="email"
+            type={"email"}
             id="email"
             name="email"
-            value={formData.email}
-            onChange={handleOnChange}
             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            required
-          />
-          <label htmlFor="firstname">Create Password</label>
-          <input
-            type="password"
-            id="password"
-            name="password"
-            value={formData.password}
+            value={data.email}
             onChange={handleOnChange}
-            className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            required
           />
-          <label htmlFor="firstname">Address</label>
+
+          <label htmlFor="password">Password</label>
+          <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2 focus-within:outline focus-within:outline-blue-300">
+            <input
+              type={showPassword ? "text" : "password"}
+              id="password"
+              name="password"
+              className=" w-full bg-slate-200 border-none outline-none "
+              value={data.password}
+              onChange={handleOnChange}
+            />
+            <span
+              className="flex text-xl cursor-pointer"
+              onClick={handleShowPassword}
+            >
+              {showPassword ? <BiShow /> : <BiHide />}
+            </span>
+          </div>
+
+          <label htmlFor="confirmpassword">Confirm Password</label>
+          <div className="flex px-2 py-1 bg-slate-200 rounded mt-1 mb-2  focus-within:outline focus-within:outline-blue-300">
+            <input
+              type={showConfirmPassword ? "text" : "password"}
+              id="confirmpassword"
+              name="confirmPassword"
+              className=" w-full bg-slate-200 border-none outline-none "
+              value={data.confirmPassword}
+              onChange={handleOnChange}
+            />
+            <span
+              className="flex text-xl cursor-pointer"
+              onClick={handleShowConfirmPassword}
+            >
+              {showConfirmPassword ? <BiShow /> : <BiHide />}
+            </span>
+          </div>
+          <label htmlFor="address">Address</label>
           <input
-            type="text"
+            type={"text"}
             id="address"
             name="address"
-            value={formData.address}
-            onChange={handleOnChange}
             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            required
+            value={data.address}
+            onChange={handleOnChange}
           />
-          <label htmlFor="firstname">Phone Number</label>
+          <label htmlFor="phone">Phone Number</label>
           <input
-            type="number"
+            type={"tel"}
             id="phone"
             name="phone"
-            value={formData.phone}
-            onChange={handleOnChange}
             className="mt-1 mb-2 w-full bg-slate-200 px-2 py-1 rounded focus-within:outline-blue-300"
-            required
+            value={data.phone}
+            onChange={handleOnChange}
           />
-          {/* Similar input fields for other form data */}
-          <button className="w-full max-w-[150px] m-auto bg-red-500 hover:bg-red-600 cursor-pointer text-white text-xl font-medium text-center py-1 rounded-full mt-4">
-            Sign Up
+
+          <button className="w-full max-w-[150px] m-auto  bg-red-500 hover:bg-red-600 cursor-pointer  text-white text-xl font-medium text-center py-1 rounded-full mt-4">
+            Sign up
           </button>
         </form>
         <p className="text-left text-sm mt-2">
-          Already have an account?{" "}
-          <Link to="/login" className="text-red-500 underline">
-            Log In
+          Already have account ?{" "}
+          <Link to={"/login"} className="text-red-500 underline">
+            Login
           </Link>
         </p>
       </div>
     </div>
   );
-};
+}
 
 export default Signup;
